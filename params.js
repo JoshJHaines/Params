11d3ac12325f5157eb433949dc7cddae7fd0824d
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-16 14:39:10 -0500

    created second function with a Param. Called function

--- params.js
+++ params.js
@@ -1,8 +1,18 @@
-let x = 5
+let x = 5;
 
-function addToX(){
-    x = x + 5
+function add5() {
+	x = x + 5;
 }
+// 5 is hardcoded into the function
+add5();
+x;
 
-addToX()
+function addToX(someNum) {
+    x = x + someNum
+}
+//adding the Param someNum into the function, makes dynamic
+
+addToX(8)
 x
+
+//by calling the variable in the function, you can define the variable when calling the function
